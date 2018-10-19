5d022f061927b62066029b2575cf1c9ba3ef5f6d
Wolfgang Wiese <xwolf@xwolf.de>
2018-10-19 13:44:14 +0200

    Fehlerfall-Ausgaben

---
+++ src/RRZESearch/Infrastructure/Templates/Results/Error-shortcode.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+
+<div class="search-error-notice">
+    <p class="attention">
+	    <strong><?php _e('The search could not be performed.','rrze-search'); ?></strong>
+    </p>
+</div>
+<div class="search-error-image"></div>
+					
