4e5dffeea52925c71205d10712e31eccf185133f
Wolfgang Wiese <xwolf@xwolf.de>
2018-10-19 13:44:42 +0200

    Korrektur bei Sprachangaben

--- src/RRZESearch/Infrastructure/Templates/Results/WordPressSearch-shortcode.php
+++ src/RRZESearch/Infrastructure/Templates/Results/WordPressSearch-shortcode.php
@@ -1,8 +1,7 @@
 <?php
 ?>
 
-<h2>Suchergebnisse</h2>
-<p class="notice-hinweis"><?= __('Es wird nur in diesem Webauftritt gesucht. Um Dokumente und Seiten aus anderen Webauftritten zu finden, nutzen Sie die jeweils dort zu findende Suchmaschine oder verwenden eine Internet-Suchmaschine.', 'fau'); ?></p>
+<h2><?php __('Search results','rrze-search'); ?></h2>
 
 <div id="resultStats"><?php echo sprintf(__('About %1$s results', 'rrze-search'),
         \count($results)); ?>
