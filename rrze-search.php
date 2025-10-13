532f3ee0c2dd3fd1fff4383a917407f7fca85918
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-10-13 14:46:11 +0200

    document(#71) - Add useful documentation for Developers

--- rrze-search.php
+++ rrze-search.php
@@ -2,38 +2,45 @@
 /*
 Plugin Name: RRZE Search
 Plugin URI: https://www.tollwerk.de
-description: a WordPress Search Plugin by tollwerk GmbH
-Author: tollwerk
+description: A WordPress Search Plugin originally by Tollwerk GmbH and maintained by RRZE
+Author: Tollwerk & RRZE
 Author URI: https://www.tollwerk.de
-Version: 0.2.6-0
-License: GPL2
+Version: 1.0.0
+License: GPL3
 Text Domain: rrze-search
 Domain Path: /languages
 */
 
+defined('ABSPATH') || exit;
 
-const RRZE_PHP_VERSION = '7.4';
-const RRZE_WP_VERSION = '6.0';
-
+const RRZE_PHP_VERSION = '8.4';
+const RRZE_WP_VERSION = '6.7';
 
 // Loading constants
 include_once('constants.php');
 
+// ==================================================
+// Actions and Activation / Deactivation Hooks
+// ==================================================
 
 add_action('plugins_loaded', 'rrze_search_init');
 add_action('init', 'rrze_search_textdomain');
-// WP Activation Hook
-
 
 register_activation_hook(__FILE__, 'activate_rrze_search_plugin');
-// WP Deactivation Hook
 register_deactivation_hook(__FILE__, 'deactivate_rrze_search_plugin');
 
+// ==================================================
+// Plugin Initialization, Activation and Deactivation
+// ==================================================
 
 /**
- * Init
+ * Initializes the Plugin
+ *
+ * Runs on plugins_loaded WP action and initializes the Plugin.
+ * It relies on the activation function that was run previously to setup the Options.
+ * @return void
  */
-function rrze_search_init()
+function rrze_search_init(): void
 {
     // Include composer autoloader
     if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
@@ -46,51 +53,59 @@ function rrze_search_init()
     }
 }
 
-
-
 /**
  * Plugin Activation Function
  */
 function activate_rrze_search_plugin()
 {
-    rrze_search_textdomain();
-    system_requirements();
-    // Include composer autoloader
-    if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
-        require_once(dirname(__FILE__) . '/vendor/autoload.php');
-    }
+    rrze_search_load_textdomain();
+    rrze_search_check_system_requirements();
+    rrze_search_include_autoloader();
+
     RRZE\RRZESearch\Ports\Multisearch::activate();
 }
-
-
 
 /**
  * Plugin Deactivation Function
  */
-function deactivate_rrze_search_plugin()
+function deactivate_rrze_search_plugin(): void
 {
-    // Include composer autoloader
-    if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
-        require_once(dirname(__FILE__) . '/vendor/autoload.php');
-    }
+    rrze_search_include_autoloader();
     RRZE\RRZESearch\Ports\Multisearch::deactivate();
 }
 
+// ==================================================
+// Helper functions | Autoload | System Check | Textdomain Loading
+// ==================================================
 /**
- * Load Textdomain
+ * Include the Composer Autoloader
  */
-function rrze_search_textdomain()
+function rrze_search_include_autoloader(): void
+{
+    if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+        require_once(dirname(__FILE__) . '/vendor/autoload.php');
+    }
+}
+
+/**
+ * Load the RRZE Search Textdomain for l10n
+ * @return void
+ */
+function rrze_search_load_textdomain(): void
 {
     load_plugin_textdomain('rrze-search', FALSE, sprintf('%s/languages/', dirname(plugin_basename(__FILE__))));
 }
 
 /**
- * Check Systemrequirements. We dont wont to hassle about outdated noob installations :)
+ * Checks the System Requirements and deactivates the Plugin in case of violation
+ *
+ * Checks the System Requirements for in L16,L17 defined WP- and PHP-Version.
+ * If the check fails, the Plugin is deactivated network-wide
+ * @return void
  */
-function system_requirements()
+function rrze_search_check_system_requirements(): void
 {
     $error = '';
-
     if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
         $error = sprintf(__('Your server is running PHP version %s. Please upgrade at least to PHP version %s.', 'rrze-test'), PHP_VERSION, RRZE_PHP_VERSION);
     }
@@ -99,13 +114,8 @@ function system_requirements()
         $error = sprintf(__('Your Wordpress version is %s. Please upgrade at least to Wordpress version %s.', 'rrze-test'), $GLOBALS['wp_version'], RRZE_WP_VERSION);
     }
 
-    // Wenn die Überprüfung fehlschlägt, dann wird das Plugin automatisch deaktiviert.
     if (!empty($error)) {
         deactivate_plugins(plugin_basename(__FILE__), FALSE, TRUE);
         wp_die($error);
     }
 }
-
-
-// Prevent abuse
-defined('ABSPATH') || exit;
