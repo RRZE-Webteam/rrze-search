ef144820886a6b23fa569004c33aa1fff2788926
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-10-21 12:09:15 +0200

    refactor(#71) - Enhances heading handling
    Improves heading handling in the search block by using RichText component for better editing and display.
    
    This change ensures the heading is rendered correctly with the selected heading level and allows users to edit the heading text directly within the block editor.
    
    The heading level dropdown is now only displayed when a heading is present.

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5e3e948c0ed1f8170d84');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a5da50ebef47bd626bbd');
--- src/block/edit.tsx
+++ src/block/edit.tsx
@@ -6,12 +6,13 @@ import {
 import {
   InspectorControls,
   HeadingLevelDropdown,
-  BlockControls
+  BlockControls,
+  RichText
 } from "@wordpress/block-editor"
 import {
   useBlockProps,
 } from "@wordpress/block-editor";
-import { __ } from "@wordpress/i18n";
+import {__} from "@wordpress/i18n";
 
 interface EditProps {
   attributes: {
@@ -19,57 +20,60 @@ interface EditProps {
     headingLevel: 2 | 3 | 4 | 5 | 6;
     heading: string;
   },
-setAttributes : (attributes: Partial<EditProps["attributes"]>) => void;
-blockProps: string[];
+  setAttributes: (attributes: Partial<EditProps["attributes"]>) => void;
+  blockProps: string[];
 }
 
-export default function Edit({ attributes, setAttributes}: EditProps) {
-  const { width, heading, headingLevel } = attributes;
+export default function Edit({attributes, setAttributes}: EditProps) {
+  const {width, heading, headingLevel} = attributes;
 
-  const blockProps = useBlockProps( {
-    className: `fau-global-search-wrapper fau-global-search-wrapper--${ width }`,
-  } );
+  const blockProps = useBlockProps({
+    className: `fau-global-search-wrapper fau-global-search-wrapper--${width}`,
+  });
 
-  const HEADING_LEVELS = [ 2, 3, 4, 5, 6 ];
+  const HEADING_LEVELS = [2, 3, 4, 5, 6];
 
-  const onChangeWidth = ( newWidth: "content-size" | "full-grid" ) => {
-    setAttributes( { width: newWidth } );
+  const onChangeWidth = (newWidth: "content-size" | "full-grid") => {
+    setAttributes({width: newWidth});
   };
 
-  const onChangeHeadingLevel = ( newHeadingLevel: number ) => {
-    setAttributes( { headingLevel: newHeadingLevel } );
+  const onChangeHeadingLevel = (newHeadingLevel: 2 | 3 | 4 | 5 | 6) => {
+    setAttributes({headingLevel: newHeadingLevel});
   };
 
-  const onChangeHeading = ( newHeading: string ) => {
-    setAttributes( { heading: newHeading } );
+  const onChangeHeading = (newHeading: string) => {
+    setAttributes({heading: newHeading});
   };
 
   return (
     <>
-      <BlockControls group="block">
-        <HeadingLevelDropdown
-          options={ [ 2, 3, 4, 5, 6 ] }
-          value={ headingLevel }
-          onChange={ onChangeHeadingLevel }
-        ></HeadingLevelDropdown>
-      </BlockControls>
+      {heading.length > 0 && (
+          <BlockControls group="block">
+              <HeadingLevelDropdown
+                  options={[2, 3, 4, 5, 6]}
+                  value={headingLevel}
+                  onChange={onChangeHeadingLevel}
+              ></HeadingLevelDropdown>
+          </BlockControls>
+      )}
       <InspectorControls>
-        <PanelBody title={ __( 'Content Settings', 'fau-elemental' ) }>
+        <PanelBody title={__('Content Settings', 'fau-elemental')}>
           <TextControl
-            label={ __( 'Heading', 'fau-elemental' ) }
-            value={ heading }
-            onChange={ onChangeHeading }
-            help={ __(
+            label={__('Heading', 'fau-elemental')}
+            value={heading}
+            onChange={onChangeHeading}
+            help={__(
               'Optional heading text to display above the search form',
               'fau-elemental'
-            ) }
+            )}
           />
         </PanelBody>
-        <PanelBody title={ __( 'Layout Settings', 'fau-elemental' ) }>
+        {heading.length > 0 && (
+        <PanelBody title={__('Layout Settings', 'fau-elemental')}>
           <SelectControl
-            label={ __( 'Width', 'fau-elemental' ) }
-            value={ width }
-            options={ [
+            label={__('Width', 'fau-elemental')}
+            value={width}
+            options={[
               {
                 label: __(
                   'Content Size (with search scope & advanced features)',
@@ -84,39 +88,36 @@ export default function Edit({ attributes, setAttributes}: EditProps) {
                 ),
                 value: 'full-grid',
               },
-            ] }
-            onChange={ onChangeWidth }
-            help={ __(
+            ]}
+            onChange={onChangeWidth}
+            help={__(
               'Content Size: for editorial content, includes search scope selection and advanced features. Full Grid: for wide teaser components, simple search only.',
               'fau-elemental'
-            ) }
+            )}
           />
         </PanelBody>
+      )}
       </InspectorControls>
       <div className="fau-global-search__outer-wrapper">
-        <div { ...blockProps }>
-          { width === 'full-grid' && heading && (
-            <h3 className="fau-global-search__heading fau-global-search__heading--full-grid">
-              { heading }
-            </h3>
-          ) }
-          { width !== 'full-grid' && heading && (
-            <h3 className="fau-global-search__heading">
-              { heading }
-            </h3>
-          ) }
+        <div {...blockProps}>
+          {width === 'full-grid' && heading && (
+            <RichText placeholder={__("Heading", "rrze-search")} tagName={`h${headingLevel}`} allowedFormats={[]} className="fau-global-search__heading fau-global-search__heading--full-grid" value={heading} onChange={onChangeHeading} />
+          )}
+          {width !== 'full-grid' && heading && (
+            <RichText placeholder={__("Heading", "rrze-search")} tagName={`h${headingLevel}`} allowedFormats={[]} className="fau-global-search__heading" value={heading} onChange={onChangeHeading} />
+          )}
           <form className="fau-global-search fau-global-search__form">
             <div
-              className={ `fau-global-search__input-wrapper${
+              className={`fau-global-search__input-wrapper${
                 width === 'full-grid'
                   ? ' fau-global-search__input-wrapper--full-grid'
                   : ''
-              }` }
+              }`}
             >
               <input
                 type="search"
                 className="fau-global-search__input"
-                placeholder={ __( 'Search…', 'fau-elemental' ) }
+                placeholder={__('Search…', 'fau-elemental')}
                 autoComplete="off"
                 disabled
               />
@@ -126,7 +127,7 @@ export default function Edit({ attributes, setAttributes}: EditProps) {
                 disabled
               >
 								<span className="fau-global-search__button-text">
-									{ __( 'Search', 'fau-elemental' ) }
+									{__('Search', 'fau-elemental')}
 								</span>
                 <span
                   className="fau-global-search__button-icon"
